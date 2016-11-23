c0268e69a2ac71647c6de27174ff4dc656eda342
NanFeng009 <yksring@gmail.com>
2016-11-23 12:31:29 +0800

    Create exception.java

---
+++ exception.java
@@ -0,0 +1,7 @@
+private void dumpStack(){
+Exception e = new Exception();
+StackTraceElement[] stes = e.getStackTrace();
+if(null != stes){
+for(int index=0 ; index < stes.length ; index++){
+TraceDefaults.traceInterface.debug("----------------", "",stes[index].toString());
+}
