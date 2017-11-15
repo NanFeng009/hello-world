0ab3e834bb49fe1c61d2b1d72a311453df1c6b98
NanFeng009 <yksring@gmail.com>
2017-11-15 14:56:18 +0800

    Update exception.java

--- exception.java
+++ exception.java
@@ -1,9 +1,10 @@
-private void dumpStack(){
-Exception e = new Exception();
-StackTraceElement[] stes = e.getStackTrace();
-if(null != stes){
-  for(int index=0 ; index < stes.length ; index++){
-    TraceDefaults.traceInterface.debug("----------------", "",stes[index].toString());
-  }
-}
-}
+	private void dumpStack() {
+		Exception e = new Exception();
+		StackTraceElement[] stes = e.getStackTrace();
+		if (null != stes) {
+			for (int index = 0; index < stes.length; index++) {
+				TraceDefaults.traceInterface.debug("----------------", "",
+						stes[index].toString());
+			}
+		}
+	}
