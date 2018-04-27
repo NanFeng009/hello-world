d259f78c64bfb04fffe1f11692b141cdfe5ea907
NanFeng009 <yksring@gmail.com>
2018-04-27 16:16:05 +0800

    Update string_2_hex.c

--- string_2_hex.c
+++ string_2_hex.c
@@ -2,30 +2,27 @@
  
 #include <stdio.h>
 #include <string.h>
- 
+
+void _print_hex(uint32_t src_len, uint8_t* src_str)
+{
+    int i, j;
+    int dst_len = src_len * 2 + 2;
+    unsigned char *dst_str = new unsigned char[dst_len];
+    memset(dst_str, 0, dst_len);
+    for(i = 0, j = 0; i < src_len; i++, j+=2)
+    {
+        sprintf((char*)(dst_str+j), "%02X", src_str[i]);
+    }
+    dst_str[j++] = '\0';
+    dst_str[j] = '\n';
+    printf("The length is %d\n, and date is \n%s\n", src_len, dst_str);
+    delete[] dst_str;
+}
+
 int main()
 {
-    unsigned char str[100],strH[200];
-    int i,j;
-     
-    printf("Enter string: ");
-    scanf("%[^\n]s",str);
-     
-    printf("\nString is: %s\n",str);
-     
-    /*set strH with nulls*/
-    memset(strH,0,sizeof(strH));
-     
-    /*converting str character into Hex and adding into strH*/
-    for(i=0,j=0;i<strlen(str);i++,j+=2)
-    { 
-        sprintf((char*)strH+j,"%02X",str[i]);
-    }
-    strH[j]='\0'; /*adding NULL in the end*/
-     
-    printf("Hexadecimal converted string is: \n");
-    printf("%s\n",strH);
-     
-     
+    unsigned char str[100] = "Hwllo world";
+    
+    _print_hex(100, str);
     return 0;
 }
